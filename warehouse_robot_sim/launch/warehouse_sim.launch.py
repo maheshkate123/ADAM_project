0ea43f225a81c8325d4d3eaa3ce775ee5b6ad521
reynash <yashnair7779@gmail.com>
2024-12-16 12:11:41 +0530

    Initial upload for omnidrive2 branch

---
+++ warehouse_robot_sim/launch/warehouse_sim.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+import os
+
+def generate_launch_description():
+    package_name = 'warehouse_robot_sim'
+    urdf_file_name = 'Tribot-oakd.urdf'
+
+    urdf = os.path.join(
+        get_package_share_directory(package_name),
+        'urdf',
+        urdf_file_name)
+
+    # Ensure the URDF file exists
+    if not os.path.exists(urdf):
+        raise FileNotFoundError(f'URDF file "{urdf}" does not exist!')
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen'
+        ),
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=['-entity', 'robot', '-file', urdf],
+            output='screen'
+        )
+    ])
+
