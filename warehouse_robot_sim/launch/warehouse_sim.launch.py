edb50b768f534822d83800177f6eac60051f1ba9
Pankaj <pankaj@v3smarttech.com>
2024-12-18 09:52:36 +0000

    urdf meshes issues resolved

--- warehouse_robot_sim/launch/warehouse_sim.launch.py
+++ warehouse_robot_sim/launch/warehouse_sim.launch.py
@@ -1,11 +1,13 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
     package_name = 'warehouse_robot_sim'
-    urdf_file_name = 'Tribot-oakd.urdf'
+    urdf_file_name = 'TriOrb300P100.urdf'
+    
 
     urdf = os.path.join(
         get_package_share_directory(package_name),
