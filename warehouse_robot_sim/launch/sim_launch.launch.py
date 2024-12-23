7f8ac31b22adfe45a17220c52d4729c1b18807d0
reynash <yashnair7779@gmail.com>
2024-12-23 14:35:33 +0530

    Robot Spawning in Gazebo

--- warehouse_robot_sim/launch/sim_launch.launch.py
+++ warehouse_robot_sim/launch/sim_launch.launch.py
@@ -1,46 +1,35 @@
-# Copyright 2022 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
-from launch.event_handlers import OnProcessExit
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, SetEnvironmentVariable
 from launch_ros.actions import Node
-from launch.substitutions import PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 import xacro
 
 
 def generate_launch_description():
 
+      
     world = os.path.join(get_package_share_directory(
-        'warehouse_robot_sim'), 'worlds', 'small_warehouse.sdf')    
+        'warehouse_robot_sim'), 'worlds', 'warehouse.world')
+    
  
     spawn_x_val = '0.0'
     spawn_y_val = '0.0'
     spawn_z_val = '0.025'
     spawn_yaw_val = '0.0'
     
+
+
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),launch_arguments={'world':world}.items()
              )
+
     robot_path = os.path.join(
         get_package_share_directory('warehouse_robot_sim'))
 
@@ -48,33 +37,45 @@ def generate_launch_description():
                               'urdf',
                               'TriBot_Oakd.urdf.xacro')
 
+    # Parse the xacro file
     doc = xacro.parse(open(xacro_file))
     xacro.process_doc(doc)
     params = {'robot_description': doc.toxml()}
 
+    # Define nodes
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params,{'publish_frequency': 50.0}]
+        parameters=[params, {'publish_frequency': 50.0}]
     )
 
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'TriOrb300P100',
-                                    '-x', spawn_x_val,
-                                    '-y', spawn_y_val,
-                                    '-z', spawn_z_val,
-                                    '-Y', spawn_yaw_val],
-                        output='screen')
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', 'TriOrb300P100',
+            '-x', spawn_x_val,
+            '-y', spawn_y_val,
+            '-z', spawn_z_val,
+            '-Y', spawn_yaw_val
+        ],
+        output='screen'
+    )
 
-
-    rviz2= ExecuteProcess(cmd=['rviz2'],
-            output="screen")
+    rviz2 = ExecuteProcess(cmd=['rviz2'], output="screen")
 
     return LaunchDescription([
+        # Set the GAZEBO_MODEL_PATH environment variable
+        SetEnvironmentVariable(
+            name='GAZEBO_MODEL_PATH',
+            value=os.path.join(robot_path, "models")
+        ),
         gazebo,
         spawn_entity,
         node_robot_state_publisher,
+        # Uncomment RViz if needed
         # rviz2
-         ])
+    ])
+
--- warehouse_robot_sim/readme.md
+++ warehouse_robot_sim/readme.md
@@ -3,8 +3,3 @@
 sudo apt update
 sudo apt install gazebo
 sudo apt install ros-humble-gazebo-ros
-<<<<<<< HEAD
-
-sudo apt install ros-humble-gazebo-ros-pkgs ros-humble-gazebo-ros
-=======
->>>>>>> 2349e1b (Removed config error)
