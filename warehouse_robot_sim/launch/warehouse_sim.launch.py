47c9bff1e6a92cf627b172a6d82e62d2ee56fba1
maheshkate <mahesh.kate@emagegroup.com>
2024-12-23 16:20:29 +0800

    previous commit ++

--- warehouse_robot_sim/launch/warehouse_sim.launch.py
+++ warehouse_robot_sim/launch/warehouse_sim.launch.py
@@ -2,11 +2,13 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import AppendEnvironmentVariable
 import os
 
 def generate_launch_description():
     package_name = 'warehouse_robot_sim'
     urdf_file_name = 'TriOrb300P100.urdf'
+    triorb_pkg_path = get_package_share_directory('warehouse_robot_sim')
     
 
     urdf = os.path.join(
@@ -19,6 +21,10 @@ def generate_launch_description():
         raise FileNotFoundError(f'URDF file "{urdf}" does not exist!')
 
     return LaunchDescription([
+        AppendEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=os.join(triorb_pkg_path, "models")),
+
         ExecuteProcess(
             cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
             output='screen'
