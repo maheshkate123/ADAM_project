825a3e4e08ba10752c6c7598cadf4f7080d4ae0b
reynash <yashnair7779@gmail.com>
2025-01-08 12:36:17 +0530

    Demo move package created

---
+++ image_demo_movements/launch/move.launch.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='image_demo_movements',
+            executable='move_square.py',
+            name='move_node',
+            output='screen',
+            parameters=[{
+                
+            }],
+        ),
+    ])
